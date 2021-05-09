ad3eae09ef44a8df36783db6f477ab46d9bcbc58
Dat Phan <phanthanhdatftu@gmail.com>
2021-05-09 23:12:18 +0700

    add filter function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import Header from "components/Header";
 import ProductFeature from "features/Product";
-import { useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
-import productApi from "./api/productApi";
 import "./App.css";
 import NotFound from "./components/NotFound";
 import AlbumFeature from "./features/Album";
@@ -10,16 +8,6 @@ import CounterFeature from "./features/Counter";
 import TodoFeature from "./features/Todo";
 
 function App() {
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const params = {
-        _limit: 10,
-      };
-      const productList = await productApi.getAll(params);
-      console.log(productList);
-    };
-    fetchProducts();
-  }, []);
   return (
     <div>
       <Header />
